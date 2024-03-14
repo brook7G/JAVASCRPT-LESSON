93ae886f8ca8f038ddd2416a8793017454a7c5c7
brook7g <birukefekadu632@gmail.com>
2024-03-14 19:35:12 +0300

    adding answers for the questions added last time

--- lesson4/QUIZES/practice.js
+++ lesson4/QUIZES/practice.js
@@ -1,3 +1,84 @@
 console.log(">>>>>>>>>>>>>>>PRACTICE SECTION");
 
 console.log("ANSWER FOR QUESTION-1:>>>>>>>")
+
+let isMan = true;
+let myAge  =24;
+let earthAge = 1234567891234567n;
+let myName = "Biruk Fekadu";
+let myEmail = undefined ;
+
+console.log(`The type of variable 'isMan' is: ${typeof isMan}`);
+console.log(`The type of variable 'myAge' is: ${typeof myAge}`);
+console.log(`The type of variable 'earthAge' is: ${typeof earthAge}`);
+console.log(`The type of variable 'myName' is: ${typeof myName}`);
+console.log(`The type of variable 'myEmail' is: ${typeof myEmail}`);
+
+
+                    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>ANSWER FOR QUESTION-2:>>>>");
+
+                    let isMan2 = true;
+                    let myAge2  =24;
+                    let earthAge2 = 1234567891234567n;
+                    let myName2 = "Biruk Fekadu";
+                    let myEmail2 = undefined;
+
+                    console.log(`${isMan2 } [${typeof isMan2}]`);
+                    console.log(`${myAge2 } [${typeof myAge2}]`);
+                    console.log(`${earthAge2 } [${typeof earthAge2}]`);
+                    console.log(`${myName2 } [${typeof myName2}]`);
+                    console.log(`${myEmail2 } [${typeof myEmail2}]`);
+
+
+console.log(">>>>>>>ANSWER FOR QUESTION-3:>>>>");
+
+let string1 = "1234";
+console.log(`${string1} [${typeof string1}]`);
+
+
+let numberString1 = Number(string1);
+console.log(`${typeof numberString1} [${numberString1}] `);    
+
+
+let  bigNumber=BigInt(numberString1);
+console.log(`${typeof bigNumber} [${bigNumber}]`);
+
+
+let boolBig = Boolean(bigNumber);
+console.log(`${typeof boolBig} [${boolBig}]`);
+
+
+                    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>ANSWER FOR QUESTION-4:>>>>>>>");
+
+                    let addString = "Biruk" + "Fekadu";
+                    console.log(`${addString} [${typeof addString}]`);
+
+                    let addNumber = 19 + 99;
+                    console.log(`${addNumber} [${typeof addNumber}]`);
+                    
+                    let addBool = true + false;
+                    console.log(`${addBool} [${typeof addBool}]`);
+
+                    let addBool2 = true + true;
+                    console.log(`${addBool2} [${typeof addBool2}]`);
+
+                    let addBool3 = false + false;
+                    console.log(`${addBool3} [${typeof addBool3}]`);
+
+                    let addBigInt = 1234567887654n + 123654327890n;
+                    console.log(`${addBigInt} [${typeof addBigInt}]`);
+
+                    let addUndefined = undefined + undefined;
+                    console.log(`${addUndefined} [${typeof addUndefined}]`);
+
+
+                    let addNull = null + null;
+                    console.log(`${addNull} [${typeof addNull}]`);
+
+                    
+
+
+
+
+
+
