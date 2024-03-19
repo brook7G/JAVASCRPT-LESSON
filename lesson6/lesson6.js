0439dcf97c4f5a83f101f26de81848984e4d5ea0
brook7g <birukefekadu632@gmail.com>
2024-03-19 19:07:21 +0300

    adding b. instanceOf  topic to lesson 6

--- lesson6/lesson6.js
+++ lesson6/lesson6.js
@@ -1,5 +1,7 @@
 console.log("THIS IS LESSON-6: GOING THROUGH METHODS ");
 
+
+
 console.log(">>>>>>>>>>>>>>>>>>>> A. LENGTH: ");
 
 let regionsArray = ["OROMIA", "TIGRAI", "SNNP", "AFAR", "GAMBELA", "B-GUMUZ", "AMHARA"];
@@ -11,7 +13,7 @@ regionsArray[9] = "FINFINNE";
 
 console.log(`The length of 'regionsArray' is :${regionsArray.length}`);// 10......HOW????????????
 
-console.log(regionsArray);//(10) ['OROMIA', 'TIGRAI', 'SNNP', 'AFAR', 'GAMBELA', 'B-GUMUZ', 'AMHARA', empty × 2, 'FINFINNE']
+console.log(regionsArray);//>>>>>>>>(10) ['OROMIA', 'TIGRAI', 'SNNP', 'AFAR', 'GAMBELA', 'B-GUMUZ', 'AMHARA', empty × 2, 'FINFINNE']
                             
         //there is two empty *2 ..indexs index-7 and index -8
         console.log(regionsArray[7]);//undefined
@@ -20,6 +22,21 @@ console.log(regionsArray);//(10) ['OROMIA', 'TIGRAI', 'SNNP', 'AFAR', 'GAMBELA'
         console.log(regionsArray[4]);//GAMBELA
         console.log(regionsArray[8]);//undefined
         console.log(regionsArray[5]);//B-GUMUZ    
+
+
+                                                console.log(">>>>>>>>>>>>>>>>>>>> A. INSTANCE-OF: ");
+
+                                                let arrayDepartments = 
+                                                        ["computer science", "software engineering", "information technology", "information science"];
+
+                                                                console.log(arrayDepartments);
+                                                                console.log(arrayDepartments.indexOf("computer science"));//>>> 0
+                                                                console.log(arrayDepartments.indexOf("software engineering"));//>>>>> 1
+                                                                console.log(arrayDepartments.indexOf("information science"));//>>>> 3
+                                                                console.log(arrayDepartments.indexOf("information technology"));//>>>>> 2
+
+
+
 
         
 
