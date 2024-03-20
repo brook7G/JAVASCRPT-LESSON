9d112229bcf7bec72f1805915a020be87ecbfa3f
brook7g <birukefekadu632@gmail.com>
2024-03-20 19:07:39 +0300

    adding push, pop, shift, unshift, reverse topics and examples for each topic

--- lesson6/lesson6.js
+++ lesson6/lesson6.js
@@ -40,13 +40,14 @@ console.log(regionsArray);//>>>>>>>>(10) ['OROMIA', 'TIGRAI', 'SNNP', 'AFAR', '
 console.log(">>>>>>>>>>>>>>>>>>>> C. PUSH: ");
 arrayDepartments = arrayDepartments = ["computer science", "software engineering", "information technology", "information science"];
 console.log(arrayDepartments);// >>> (4) ['computer science', 'software engineering', 'information technology', 'information science']
-console.log(arrayDepartments.length);// >>> 4
+console.log('the length of array departments is:', arrayDepartments.length);// >>> 4
 
-// pushing new vaalue in to the existing array
+// pushing or adding new department in to the last of the existing array
 arrayDepartments.push("Aerospace engineering");
 
 console.log(arrayDepartments);// >>>['computer science', 'software engineering', 'information technology', 'information science', 'Aerospace engineering']
-console.log(arrayDepartments.length);//>>>> 5
+console.log('the length of array departments is:', arrayDepartments.length);//>>>> 5
+console.log('Index of Aerospace engineering is :', arrayDepartments.indexOf("Aerospace engineering"));////>>> 4
 
 
 //PUSHING MORE THAN TWO VALUES AT SAME TIME INTO EMPTY ARRAY 
@@ -54,12 +55,74 @@ let arraySM =[];
 
 arraySM.push('linkedIn','instagram','facebook','twitter','skool');
 console.log(arraySM);//>>> ['linkedIn', 'instagram', 'facebook', 'twitter', 'skool']
-console.log(arraySM.length);//>>> 5
+console.log('the length of arraySM is:', arraySM.length);//>>> 5
 
 
-console.log(">>>>>>>>>>>>>>>>> D. UNSHIFT: ");
+                                                                                       
 
 
+                                                        console.log(">>>>>>>>>>>>>>>>> D. UNSHIFT: ");
+
+                                                        let fruitsOnShelf = ["apple","mangos","orange","banana","lemon","avocado"];
+
+                                                        console.log(fruitsOnShelf);//>> ['apple', 'mangos', 'orange', 'banana', 'lemon', 'avocado']
+                                                        console.log('the length of fruits on shelf array is:', fruitsOnShelf.length);//>>> 6
+
+                                                        // unshift or adding a new fruit to the beginning [0] of the  index 
+                                                        fruitsOnShelf.unshift("pome");
+
+                                                        console.log(fruitsOnShelf);
+                                                        console.log('length of the fruits on shelf :',fruitsOnShelf.length);//>>> 7
+                                                        console.log('Index of the new added fruit[pome] is :', fruitsOnShelf.indexOf("pome"));//>>> 0
+                                                        console.log('Index of the fruit[orange] is :', fruitsOnShelf.indexOf("orange"));//>>> 3
+                                                        console.log('Index of the fruit[pine apple] is :', fruitsOnShelf.indexOf("pine apple"));// >> -1
+
+
+
+
+
+console.log(">>>>>>>>>>>>>>>>> E. POP: ");
+
+console.log('BEFORE pop is called : ',fruitsOnShelf);
+console.log('the LENGTH of the array before pop is called : ',fruitsOnShelf.length);//>>>> 7
+
+//poping or removing the  desired fruit  or index  from the array
+const poppedIndex  = fruitsOnShelf.pop();
+
+console.log('this is the popped fruit from the array: [',poppedIndex,']');//>>>> avocado : the last index    
+console.log('AFTER pop is called : ',fruitsOnShelf);
+console.log('the LENGTH of the array after pop is called : ',fruitsOnShelf.length);//>>>> 6
+
+
+
+
+
+                                                        console.log(">>>>>>>>>>>>>>>>> F. SHIFT: ");
+
+                                                        console.log('BEFORE shift is called: ', fruitsOnShelf);
+                                                        console.log('the LENGTH of the array before shift is called:', fruitsOnShelf.length);//>>>
+
+                                                        const shiftedIndex = fruitsOnShelf.shift();
+                                                        console.log('AFTER shift is called: ',fruitsOnShelf);
+                                                        console.log('the LENGTH of the array after shift is called: ',fruitsOnShelf.length);//>>>>
+                                                        console.log('this is the fruit shifted from the array:[',shiftedIndex,']');
+
+
+
+
+console.log(">>>>>>>>>>>>>>>>> G. REVERSE: "); 
+
+console.log('BEFORE reverse is called: ', fruitsOnShelf);//>>> ['apple', 'mangos', 'orange', 'banana', 'lemon']
+
+const reversedArray =  fruitsOnShelf.reverse();
+console.log('AFTER reverse is called:  ',fruitsOnShelf);// >>> ['lemon', 'banana', 'orange', 'mangos', 'apple']
+
+
+                                                        
+
+
+
+                                                        
 
 
 
