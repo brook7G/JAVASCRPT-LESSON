738bd64bdc89d88f9b275b1c74b48f75063d2c26
brook7g <birukefekadu632@gmail.com>
2024-03-22 16:42:24 +0300

    this is JAVASCRIPT file that contains the answer for the questions provided by html file

---
+++ lesson6/QUIZES-2/practice2.js
@@ -0,0 +1,80 @@
+console.log(">>>>>>>>>>>>>>>>>>>>SECTION PRACTICE:");
+
+                console.log("PART-1:OBJECTS >>>>>");
+                    console.log("ANSWER FOR QUESTION-1:");
+
+                    const ticket = {
+                        name:'Biruk Fekadu',
+                        sex:'male',
+                        from: 'Addis Ababa',
+                        to:'Jimma',
+                        price:3600
+                    };
+                    console.log(`Mr. ${ticket.name} is a ${ticket.sex} passenger and going from ${ticket.from} to ${ticket.to} and have paid ${ticket.price} ETB`);
+                    console.log(`passenger: ${ticket.name}`);
+                    console.log(`sex: ${ticket.sex}`);
+                    console.log(`from: ${ticket.from}`);
+                    console.log(`to: ${ticket.to}`);
+                    console.log(`price: ${ticket.price}`);
+
+
+                    console.log("ANSWER FOR QUESTION-2:");
+
+                    const person = {};
+
+                    person.name = 'BIRUK';
+                    person.surname = 'FEKADU';
+
+                    console.log(`personal information: ${person}`);
+                    console.log(`user name:${person.name}`);
+                    console.log(`user surname:${person.surname}`);
+
+                    console.log(`your name is ${person.name}, and your sur name is ${person.surname}`);
+                    console.log(`your full name is ${person.name}${person.surname}`);
+
+
+console.log("PART-2:ARRAYS >>>>>");
+    console.log("ANSWER FOR QUESTION-3:");
+
+    const books = [
+        {
+            title:'Speaking JavaScript',
+            author:'Axel Rauschmayer',
+            pages:460
+        },
+
+        {
+            title:'Programming JavaScript Applications',
+            author:' Eric Elliott',
+            pages:254
+        },
+
+        {
+            title:'understanding ECMAScript 6',
+            author:'Nicholas C. Zakas',
+            pages:352
+        }
+    ];
+    console.log(books);
+
+    console.log(books[0].title);
+    console.log(books[0].author);
+    console.log(books[0].pages);
+
+    console.log(books[1].title);
+    console.log(books[1].author);
+    console.log(books[1].pages);
+
+    console.log(books[2].title);
+    console.log(books[2].author);
+    console.log(books[2].pages);
+   
+    
+  
+
+
+
+
+
+
+
