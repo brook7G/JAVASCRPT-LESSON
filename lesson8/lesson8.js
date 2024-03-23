6756f50da194dc5603eca94df4b8e140063344db
brook7g <birukefekadu632@gmail.com>
2024-03-23 19:13:35 +0300

    adding code of assigning and arithmetic operation

--- lesson8/lesson8.js
+++ lesson8/lesson8.js
@@ -1,1 +1,28 @@
-console.log("LESSSON-8 COMING SOON");
+/*use strict*/
+console.log(">>>>>>>>>>>>>OPERATOS:");
+
+
+        console.log("1: ASSIGNMENT OPERATORS");
+
+            const finishedLessons = 7;
+            console.log("WE HAVE LEARNED: ", finishedLessons," LESSONS");
+
+            /*If several assignment operators appear in a sequence, the order from right to left applies. So the sequence*/
+
+            var thisLesson = 8;
+            console.log("WE ARE LEARNING LESSON: ",thisLesson);
+
+            var newLesson = thisLesson = 9;
+            console.log("OUR NEXT LESSON WILL BE LESSON: ",newLesson);
+
+            //MEANS THE SAME AS:
+            var thisLesson = 8;
+            console.log("WE ARE LEARNING LESSON: ",thisLesson);
+            
+                thisLesson = 9;
+
+                var newLesson = thisLesson ;
+                console.log("OUR NEXT LESSON WILL BE LESSON: ",newLesson);
+
+
+
