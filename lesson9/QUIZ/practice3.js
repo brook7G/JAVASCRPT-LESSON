db40ccf483b788addf90733e9a836f212a3e78cf
brook7g <birukefekadu632@gmail.com>
2024-03-30 14:46:28 +0300

    uploading arithmetic, comparison, logical operators sample practice

---
+++ lesson9/QUIZ/practice3.js
@@ -0,0 +1,56 @@
+console.log("SAMPLE QUESTIOMNS TO PRACTICE: REPLACE THE UNDERSCORE WITH APPROPRIATE OPERATOR ");
+//
+//1. QUESTION
+console.log("");
+
+console.log("2_3_1 is : "); // expected == 7
+console.log("2_4 is : "); // expected == 16
+console.log("5_1 is : "); // expected == 5
+console.log("8_2_5_2 is : "); // expected == 39
+
+//
+//ANSWER
+console.log("");
+    console.log(2 * 3 + 1);
+    console.log(2 ** 4);
+    console.log(5 * 1);
+    console.log(8 ** 2 - 5 ** 2);
+//
+// 2. QUESTION
+console.log("");
+
+console.log("4 * 5 _ 20 is : ");
+console.log('6 * 5 _ "30" is : ');
+console.log("-17 _ 0 is : ");  
+console.log("25 _ 1 is : ");  
+console.log("2 + 2 * 2 _ 4 is : ");
+
+//
+//ANSWER 
+console.log("");
+    console.log(4 * 5 === 20);
+    console.log(6 * 5 == "30");
+    console.log( -17 < 0);
+    console.log(25 > 1);
+    console.log(2 + 2 * 2 != 4)
+
+//
+//3. QUESTION
+console.log("");
+
+console.log("true _ false is : ");  
+console.log("false _ false is : ");  
+console.log("false _ false _ true is : ");  
+console.log("true _ false _ false && true is : ");
+
+//
+//ANSWER 
+console.log("");    
+    console.log(true || false);
+    console.log(false || !false);
+    console.log(false && false || true);
+    console.log(true || false && false && true)
+
+
+
+
