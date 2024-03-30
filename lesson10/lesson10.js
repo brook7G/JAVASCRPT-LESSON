f38b50d186a7c29fe289faee0ef40b13006f092c
brook7g <birukefekadu632@gmail.com>
2024-03-30 18:55:48 +0300

    string, comparison and other js operators

---
+++ lesson10/lesson10.js
@@ -0,0 +1,43 @@
+console.log("STRING OPERATORS:");
+console.log("");
+//
+//
+const selamta = "Selam";
+console.log(selamta + " " + "Hundee !!");// Selam Hundee !!
+//
+//
+const melikt = "Melkam Addis Amet "
+const melkamMignot = melikt + 2017;
+console.log(melkamMignot);//  Melkam Addis Amet 2017
+//
+console.log(typeof melkamMignot); // STRING
+//
+//
+console.log("");
+console.log("compound assignment operators:")
+//
+//
+let weregna = "Mn malet felegeh nw "
+weregna += "biruk ";
+weregna += 101;
+//
+console.log(weregna);
+//
+//
+//
+console.log("");
+                    console.log("compound assignment operators");
+                    //
+                        console.log(10 === 5); // false
+                        console.log(10 === 10); // true
+                        console.log(10 === 10n); // false
+                        console.log(10 === "10"); // false
+                        console.log("10" === "10"); // true
+                        console.log(0 === false); // false
+                        console.log("biruk" === "fekadu"); // false 
+                        console.log(undefined === false); // false 
+                        console.log(null === false); // false
+
+
+
+
