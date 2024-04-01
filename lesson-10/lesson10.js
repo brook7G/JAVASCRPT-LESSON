b46092e137d2084d50be94dcb236648fe7d774ec
brook7g <birukefekadu632@gmail.com>
2024-04-01 18:33:35 +0300

    precedence of operators in js

--- lesson-10/lesson10.js
+++ lesson-10/lesson10.js
@@ -155,7 +155,18 @@ console.log("");
                 //\\//\\//\\//\\//\\//\\//\\
                 ////\\//\\//\\//\\//\\//\\//\\
 
-console.log("PRECEDENCE")                
+
+
+console.log("");                
+
+console.log("PRECEDENCE");
+
+        let a = 10;
+        let b = a + 2 * 3;
+        let c = a + 2 < 20 -15;
+        console.log(a); // 10
+        console.log(b); // 16 
+        console.log(c); // false
 
 
 
