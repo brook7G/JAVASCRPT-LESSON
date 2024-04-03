559bd00e16ed30c29378aa71b10ea4e73e08aea8
brook7g <birukefekadu632@gmail.com>
2024-04-03 18:46:27 +0300

    dialog box (alert, confirm, prompt) are added

--- lesson-11/lesson11.js
+++ lesson-11/lesson11.js
@@ -1,1 +1,92 @@
 console.log("INTERACTING WITH THE USER");
+    console.log("");
+
+    console.log("1.dialog box:");
+        /*alert("what are you?");
+        window.alert("You dont have girlfriend and you are broke programmer!");
+        alert(true);
+        alert(24*1);
+        alert("text 1","text 2"); //only "text 1" will be displayed*/
+
+        // run the above program and check your browser
+
+    console.log("");
+    console.log("2.confirm dialog box:");
+        /*let confirmation = window.confirm("ARE YOU READY TO START THIS LESSON? ");
+        console.log(singularity); */
+
+                    //run the above program and check the popuP on your browser screen
+
+       /* let remove = confirm("Remove all data");
+        let message = remove ? "deleting data" : "cancelled"
+
+        console.log(message); */
+        
+        
+        
+                    //run the above program and check the popuP on your browser screen
+    console.log("");
+    console.log("3.prompt dialog box:");
+
+        /*let name = window.prompt("what is your name?" ,"retarded");
+        name = name? name: "anonymous";
+        let age = prompt("hello " + name + " how old are you?");
+        alert(name + " is " + age + " years old and still single!!");*/
+console.log("");
+console.log("ANSWER FOR THE PRACTICE QUESTION ");
+//
+//
+
+    /*let length= prompt("what is the length of the box?" ,"enter LENGTH of the box");       
+
+    let width=prompt("what is the width of the box?" ,"enter WIDTH of the box");
+
+    let height = prompt("what is the height of the box?" ,"enter HEIGHT of the box");
+
+    let boxVolume = length * width * height;
+    alert('the volume of the box is '+ boxVolume +' meter cube'); */
+
+//
+console.log("");
+//
+console.log("ANSWER FOR LAB SESSION QUESTION");
+//
+
+    /*let contacts = [{
+        name: "Maxwell Wright",
+        phone: "(0191) 719 6495",
+        email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+        }, {
+        name: "Raja Villarreal",
+        phone: "0866 398 2895",
+        email: "posuere.vulputate@sed.com"
+        }, {
+        name: "Helen Richards",
+        phone: "0800 1111",
+        email: "libero@convallis.edu"
+        }];
+        
+        
+        
+        // write your code here
+        
+        let newUser = {
+            name:"",
+            phone:"",
+            email:""
+        };
+        newUser.name = prompt("what is your name");
+        newUser.phone = prompt("what is your phone number");
+        newUser.email = prompt("what is your email address"); 
+        
+        contacts.push(newUser);
+        let last = contacts.length - 1;
+        
+        console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+        console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);*/
+
+
+        
+
+
+
