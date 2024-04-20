480243af109deea7ef24a7e17992a2612320eaac
brook7g <birukefekadu632@gmail.com>
2024-04-20 13:45:49 +0300

    Add new temp_mean function

--- lesson-17/lesson17.js
+++ lesson-17/lesson17.js
@@ -61,6 +61,39 @@ function get_temp_mean() {
   }
   temp_mean = sum_of_Temperature / temperature.length;
 }
+//
+//
+console.log("CALLING FUNCTIONS:");
+console.log("");
+//
+//
+let temperature_2;
+let sum_of_Temperature_2;
+let temp_mean_2;
+
+function get_temp_mean_2() {
+  sum_of_Temperature_2 = 0;
+  for (let i = 0; i < temperature_2.length; i++) {
+    sum_of_Temperature_2 += temperature_2[i];
+  }
+  temp_mean_2 = sum_of_Temperature_2 / temperature_2.length;
+}
+//
+//
+temperature_2 = [
+  12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21,
+  20, 19, 17, 16,
+];
+get_temp_mean_2();
+console.log(`mean : ${temp_mean_2}`); // mean : 16.666666666666668
+//
+temperature_2 = [
+  17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24,
+  21, 19, 18, 17, 16,
+];
+get_temp_mean_2();
+console.log(`mean : ${temp_mean_2}`); //mean : 18.083333333333332
+
 
 
 
