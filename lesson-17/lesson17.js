e06ba1fcf9cd80691042082404691307dc0bfc94
brook7g <birukefekadu632@gmail.com>
2024-04-20 18:50:39 +0300

    Refactor: Refactored function declaring syntax and examples

--- lesson-17/lesson17.js
+++ lesson-17/lesson17.js
@@ -42,27 +42,27 @@ console.log(`mean : ${mean_Temp}`); // mean : 18.083333333333332
 //
 //
 //
-                                    console.log("DECLARING FUNCTIONS:");
-                                    console.log("")
-                                    //
-                                    //declare variables
+console.log("DECLARING FUNCTIONS:");
+console.log("");
+//
+//declare variables
 
-                                    let temperature;
-                                    let sum_of_Temperature ;
-                                    let temp_mean;
+let temperature;
+let sum_of_Temperature;
+let temp_mean;
 
-                                    //
-                                    //declare function
+//
+//declare function
 
-                                    function get_temp_mean() {
-                                      sum_of_Temperature = 0;
-                                      for (let i = 0; i < temperature.length; i++) {
-                                        sum_of_Temperature += temperature[i];
-                                      }
-                                      temp_mean = sum_of_Temperature / temperature.length;
-                                    }
-                                    //
-                                    //
+function get_temp_mean() {
+  sum_of_Temperature = 0;
+  for (let i = 0; i < temperature.length; i++) {
+    sum_of_Temperature += temperature[i];
+  }
+  temp_mean = sum_of_Temperature / temperature.length;
+}
+//
+//
 console.log("CALLING FUNCTIONS:");
 console.log("");
 //
@@ -96,36 +96,36 @@ console.log(`mean : ${temp_mean_2}`); //mean : 18.083333333333332
 //
 //
 
-                                      console.log("");
-                                      console.log('declaring FUNCTIONS before they are called');
-                                      //
-                                      //
+console.log("");
+console.log("declaring FUNCTIONS before they are called");
+//
+//
 
-                                      let name = 'Biruk Fekadu';
-                                      //
-                                      //declaring before calling
+let name = "Biruk Fekadu";
+//
+//declaring before calling
 
-                                      function get_user_info(){
-                                        console.log(`user-name : ${name}`);
-                                      }
-                                      //
-                                      //calling the function
+function get_user_info() {
+  console.log(`user-name : ${name}`);
+}
+//
+//calling the function
 
-                                      get_user_info(); // user-name : Biruk Fekadu
-                                      //
+get_user_info(); // user-name : Biruk Fekadu
+//
 
-                                      console.log("");
-                                      console.log('calling FUNCTIONS before they are declared');
-                                      //WILL WORK EXACTLY THE SAME AS :
+console.log("");
+console.log("calling FUNCTIONS before they are declared");
+//WILL WORK EXACTLY THE SAME AS :
 
-                                      let user_name = "Fekadu Tolesa";
-                                      get_user_name(); // user-name : Fekadu Tolesa
-                                      //
-                                      function get_user_name() {
-                                        console.log(`user-name : ${user_name}`);
-                                      };
-                                      //
-                                      //
+let user_name = "Fekadu Tolesa";
+get_user_name(); // user-name : Fekadu Tolesa
+//
+function get_user_name() {
+  console.log(`user-name : ${user_name}`);
+}
+//
+//
 console.log("");
 console.log("FUNCTIONS:-local variables");
 //
@@ -157,17 +157,17 @@ get_temp_mean_3();
 console.log(`mean : ${temp_mean_3}`); // mean : 18.083333333333332
 //
 //
-                                                                  console.log("");
-                                                                  console.log("THE .. return .. STATEMENT");
-                                                                  //
-                                                                  function showMsg() {
-                                                                    console.log("Message-1");
-                                                                    return;
-                                                                    console.log("Message-2");
-                                                                  }
-                                                                  showMsg(); // Message-1
-                                                                  //
-                                                                  //
+console.log("");
+console.log("THE .. return .. STATEMENT");
+//
+function showMsg() {
+  console.log("Message-1");
+  return;
+  console.log("Message-2");
+}
+showMsg(); // Message-1
+//
+//
 console.log("");
 console.log("THE .. return .. STATEMENT");
 //
@@ -177,12 +177,37 @@ function getTrue() {
 //
 let test = getTrue();
 console.log(test); // true
+//
+//
+console.log("");
+console.log("...return .. on previous example");
+//
+let temperature_4;
+let temp_mean_4;
+//
+function get_temp_mean_4() {
+  let sum_of_Temperature_4 = 0;
+  let result;
+  for (let i = 0; i < temperature_4.length; i++) {
+    sum_of_Temperature_4 += temperature_4[i];
+  }
+  result = sum_of_Temperature_4 / temperature_4.length;
 
+  return result;
+}
+//
+temperature_4 = [
+  12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21,
+  20, 19, 17, 16,
+];
 
+temp_mean_4 = get_temp_mean_4();
+console.log(`mean---: ${temp_mean_4}`); // mean---: 16.666666666666668
+//
+temperature_4 = [
+  17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24,
+  21, 19, 18, 17, 16,
+];
 
-
-
-
-
-
-
+temp_mean_4 = get_temp_mean_4();
+console.log(`mean---: ${temp_mean_4}`); // mean---: 18.083333333333332
