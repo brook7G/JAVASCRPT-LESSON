576df8f58aa17e359d682f5263f62d6bd2418a80
brook7g <birukefekadu632@gmail.com>
2024-04-21 23:31:56 +0300

    Refactor: Add function to calculate mean temperature for a given array

--- lesson-17/lesson17.js
+++ lesson-17/lesson17.js
@@ -281,3 +281,29 @@ let names = ["balcha", "caaltuu", "badhasa", "suleman"];
 let name_ = getElement(names, 2);
 //
 console.log(name_); // badhasa
+//
+//
+//
+console.log("");
+console.log("getElement:-temperature_mean");
+//
+//
+function get_temp_mean_7(temperature_7) {
+  let sum_of_Temperature_7 = 0;
+  for (let i = 0; i < temperature_7.length; i++) {
+    sum_of_Temperature_7 += temperature_7[i];
+  }
+  return sum_of_Temperature_7 / temperature_7.length;
+}
+//
+let day_1 = [
+  12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21,
+  20, 19, 17, 16,
+];
+console.log(`mean day1 : ${get_temp_mean_7(day_1)}`); //mean day1 : 16.666666666666668
+//
+let day_2 = [
+  17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24,
+  21, 19, 18, 17, 16,
+];
+console.log(`mean day2 : ${get_temp_mean_7(day_2)}`); // mean day2 : 18.083333333333332
