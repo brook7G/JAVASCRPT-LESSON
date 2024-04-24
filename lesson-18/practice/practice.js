ac6741de09cc863f9be29fcff67e07060d0db20a
brook7g <birukefekadu632@gmail.com>
2024-04-24 17:03:39 +0300

    Refactor: Exercise one of practice section

--- lesson-18/practice/practice.js
+++ lesson-18/practice/practice.js
@@ -1,1 +1,16 @@
 console.log("PRACTICE QUESTIONS");
+console.log("Exercise-1:");
+//
+let numbers=[50,10,40,30,20];
+function compare_numbers(a,b){
+    let returnVal = 0;
+    if(a>b){
+        returnVal = 1;
+    }else if(a<b){
+        returnVal = -1;
+    }
+    return returnVal;
+}
+//
+let sorted=numbers.sort(compare_numbers);
+console.log(sorted);
