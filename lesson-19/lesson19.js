4cfc284af1b3e8aa68904b8ac5d372319e23b292
brook7g <birukefekadu632@gmail.com>
2024-04-27 01:46:44 +0300

    Fix: multiple syntax and reference errors

--- lesson-19/lesson19.js
+++ lesson-19/lesson19.js
@@ -1,12 +1,35 @@
 console.log("Lesson-19: ERROR AND EXCEPTIONS PART-1");
 //
-let multiply = (a b) => a + b; // -> Uncaught SyntaxError: Unexpected identifier
-let result = multiply(10, 20);
-console.log(result);
+// let multiply = (a b) => a + b; // -> Uncaught SyntaxError: Unexpected identifier
+// let result = multiply(10, 20);
+// console.log(result);
 //
 //
 console.log("Next.....");
+//uncomment and check the code separately from the previous example
+// let multipl = (a, b) => a + b;
+// let result = multiply(10, 20); // -> Uncaught ReferenceError: multiply is not defined
+// console.log(result);
+
 //
-let multipl = (a, b) => a + b;
-let result = multiply(10, 20); // -> Uncaught ReferenceError: multiply is not defined
-console.log(result);
+console.log("Next.....");
+//uncomment and check the code separately from the previous example
+let multiply_ = (a, b) => a + b;
+let result_ = multiply_(10, 20);
+console.log(result_); // -> 30 ?
+//
+//
+console.log("corrected Again...");
+//
+let multiply = (a, b) => a * b;
+let result = multiply(10, 20);
+console.log(result); // -> 200
+//
+//
+
+console.log("Next.....");
+//
+console.log('abc'); // -> abc
+conole.log('def'); // -> Uncaught ReferenceError: conole is not defined
+console.log('ghi');
+
