67884dbf1ab2a03ab7cbe88ba7e6f6967b23de00
brook7g <birukefekadu632@gmail.com>
2024-04-27 01:22:46 +0300

    Fix(lesson-19): Add working syntax examples

--- lesson-19/lesson19.js
+++ lesson-19/lesson19.js
@@ -1,1 +1,12 @@
 console.log("Lesson-19: ERROR AND EXCEPTIONS PART-1");
+//
+let multiply = (a b) => a + b; // -> Uncaught SyntaxError: Unexpected identifier
+let result = multiply(10, 20);
+console.log(result);
+//
+//
+console.log("Next.....");
+//
+let multipl = (a, b) => a + b;
+let result = multiply(10, 20); // -> Uncaught ReferenceError: multiply is not defined
+console.log(result);
