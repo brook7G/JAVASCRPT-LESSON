79dc76b493d3725f057a3e627cae5892746e875e
brook7g <birukefekadu632@gmail.com>
2024-04-27 15:01:11 +0300

    Fix: Introduce syntax and reference error examples

--- lesson-19/lesson19.js
+++ lesson-19/lesson19.js
@@ -1,15 +1,15 @@
 console.log("Lesson-19: ERROR AND EXCEPTIONS PART-1");
 //
-// let multiply = (a b) => a + b; // -> Uncaught SyntaxError: Unexpected identifier
-// let result = multiply(10, 20);
-// console.log(result);
+let multiply = (a b) => a + b; // -> Uncaught SyntaxError: Unexpected identifier
+let result = multiply(10, 20);
+console.log(result);
 //
 //
 console.log("Next.....");
 //uncomment and check the code separately from the previous example
-// let multipl = (a, b) => a + b;
-// let result = multiply(10, 20); // -> Uncaught ReferenceError: multiply is not defined
-// console.log(result);
+let multipl = (a, b) => a + b;
+let result = multiply(10, 20); // -> Uncaught ReferenceError: multiply is not defined
+console.log(result);
 
 //
 console.log("Next.....");
@@ -29,16 +29,16 @@ console.log(result); // -> 200
 
 console.log("Next.....");
 //
-console.log('abc'); // -> abc
-conole.log('def'); // -> Uncaught ReferenceError: conole is not defined
-console.log('ghi');
+console.log("abc"); // -> abc
+conole.log("def"); // -> Uncaught ReferenceError: conole is not defined
+console.log("ghi");
 //
 //
 try {
-    console.log('abc'); // -> abc
-    conole.log('abc');
-} catch (error) {  
-    console.log(error.message); // -> conole is not defined 
+  console.log("abc"); // -> abc
+  conole.log("abc");
+} catch (error) {
+  console.log(error.message); // -> conole is not defined
 }
 //
 //
@@ -46,4 +46,17 @@ try {
 console.log("");
 console.log("ERROR WITHOUT EXCEPTIONS ");
 //
+//
+console.log("");
+console.log("Next ...");
+//
+console.log(100 / 0); // -> Infinity
+console.log(100 * "2"); // -> 200
+console.log(100 * "abc"); // -> NaN
 
+//
+//
+console.log("");
+console.log("Next ....");
+//
+console.log(Math.pow("abc", "def")); // -> NaN
