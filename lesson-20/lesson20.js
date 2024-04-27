d8b9a8ba17cae6b79a02a8c6e6ef138fc05d753d
brook7g <birukefekadu632@gmail.com>
2024-04-27 18:36:19 +0300

    Fix: Introduce RangeError in lesson 20 discussion

--- lesson-20/lesson20.js
+++ lesson-20/lesson20.js
@@ -28,6 +28,38 @@ someConstValue = 7; // -> Uncaught TypeError: Assignment to constant variable.
 //
 let someNumber = 10;
 someNumber.length(); // -> Uncaught TypeError: someNumber.length is not a function
+//
+//
+//
+console.log("");
+console.log("RangeError");
+//
+let testArray1 = Array(10);
+console.log(testArray1.length); // -> 10
+let testArray2 = Array(-1); // -> Uncaught RangeError: Invalid array length
+console.log(testArray2.length);
+//
+//
+//
+console.log("");
+console.log("THE try ... catch STATEMENT");
+//
+// try {
+//     // code to try
+// } catch (error) {
+//     // code to run in case of an error, which throw an exception
+// }
+//
+//
+try {
+    let a = b;
+} catch (error) {
+    console.log("Caught " + error); // -> Caught ReferenceError: b is not defined
+}
+console.log("We handled the exception!"); // -> we handled the exception!
+
+
+
 
 
 
