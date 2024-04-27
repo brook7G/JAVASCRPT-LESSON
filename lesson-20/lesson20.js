2b8d20729ef77179db28903c4237c419698d47df
brook7g <birukefekadu632@gmail.com>
2024-04-27 18:01:41 +0300

    Refactor: Improve error messages by using correct error names

--- lesson-20/lesson20.js
+++ lesson-20/lesson20.js
@@ -3,7 +3,7 @@
 console.log("ERRORS AND EXCEPTIONS : PART-2");
 //
 console.log("");
-console.log("Syntax error");
+console.log("SyntaxError");
 //
 "use strict";
 iff (true) { //-> Uncaught SyntaxError: Unexpected token '{'
@@ -11,8 +11,24 @@ iff (true) { //-> Uncaught SyntaxError: Unexpected token '{'
 }
 //
 console.log("");
-console.log("Reference Error");
+console.log("ReferenceError");
 //
-let a = b; // -> Uncaught ReferenceError: b is not defined
+let a = b; // -> Uncaught ReferenceError: b is not defined 
+//
+fun(); // -> Uncaught ReferenceError: fun is not defined
+
+//
+//
+console.log("");
+console.log("TypeError");
+//
+const someConstValue = 5;
+someConstValue = 7; // -> Uncaught TypeError: Assignment to constant variable.
+//
+//
+let someNumber = 10;
+someNumber.length(); // -> Uncaught TypeError: someNumber.length is not a function
+
+
 
 
