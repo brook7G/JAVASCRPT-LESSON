b754e4348dadc664721bcaf81ccafcd5bc741b7a
brook7g <birukefekadu632@gmail.com>
2024-05-13 23:31:50 +0300

    Refactor: Introduce 'division' function

--- lesson-20/Practice/practice.js
+++ lesson-20/Practice/practice.js
@@ -1,19 +1,34 @@
 console.log("PRACTICE-QUESTIONS");
 console.log("EXERCISE:-1");
 //
-function div(a, b) {
-    if (b === 0) {
-        throw new RangeError("Division by zero is not allowed.");
+// function div(a, b) {
+//     if (b === 0) {
+//         throw new RangeError("Division by zero is not allowed.");
+//     }
+//     return a / b;
+// }
+
+// // Test call for valid division
+// console.log(div(10, 2)); // Expected output: 5
+
+// // Test call for division by zero
+// try {
+//     console.log(div(10, 0));
+// } catch (error) {
+//     console.error(error.message); // Expected output: Division by zero is not allowed.
+// }
+//
+function division(arg1, arg2){
+    if(arg2===0){
+        throw new Error("Division by zero is not allowed.");
     }
-    return a / b;
+    return arg1/arg2;
+};
+console.log(division(10,2))
+//
+try{
+    console.log(division(10,0))
 }
-
-// Test call for valid division
-console.log(div(10, 2)); // Expected output: 5
-
-// Test call for division by zero
-try {
-    console.log(div(10, 0));
-} catch (error) {
-    console.error(error.message); // Expected output: Division by zero is not allowed.
+catch{
+    console.error(error.message)
 }
