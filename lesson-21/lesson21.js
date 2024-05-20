72630570d8d176510f7a3c957f78ee07a5ed5bb0
brook7g <birukefekadu632@gmail.com>
2024-05-20 23:20:42 +0300

    Add console logging test cases for largest()

--- lesson-21/lesson21.js
+++ lesson-21/lesson21.js
@@ -19,5 +19,10 @@ console.log("");function largest(a, b, c) {
         return c;
     }
 }
+//
+console.log(largest(1, 1, 2)); // -> 2
+console.log(largest(1, 2, 3)); // -> 3
+console.log(largest(3, 2, 1)); // -> 3
+console.log(largest(2, 2, 1)); // -> 1
 
 //
